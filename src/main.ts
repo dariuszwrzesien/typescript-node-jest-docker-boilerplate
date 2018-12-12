2a7a53e0b62032f6b0635ee70ad9eb7b02a2c054
Dariusz Wrzesien <dwrzesien@future-processing.com>
2018-12-12 21:26:56 +0100

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { Config } from './shared/config';
 
 async function main() {
-    console.log(Config.ENVIRONMENT);
+
+    // const production = process.env.ENVIRONMENT === 'production';
+    // if(!production) {
+    //     const chokidar = require('chokidar');
+    //     const watcher = chokidar.watch('./build/dist');
+    //     watcher.on('ready', () => {
+    //         watcher.on('all', () => {
+    //             console.info("Clearing build/dist/ module cache from server");
+    //             Object.keys(require.cache).forEach(function(id) {
+    //                 if (/[\/\\]build[\/\\]dist[\/\\]/.test(id)) delete require.cache[id]
+    //             })
+    //         })
+    //     })
+    // }
+
+    console.log('123asd');
     console.log(Config.TEST);
 }
 
-main().catch(err => {console.log(err)});
+main().then(() => console.log('123')).catch(err => {console.log(err)});
